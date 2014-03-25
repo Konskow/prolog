7ee527f676a921dfc6ffb54c0bd13385db86bc24
Piotr Konsek <piotr@ThinkPad-Edge-E530.(none)>
2014-03-26 00:08:46 +0100

    up

--- lab4/lab4.pl
+++ lab4/lab4.pl
@@ -1,3 +1,15 @@
+znaczy(0,zero).   
+znaczy(1,jeden).
+znaczy(2,dwa).  
+znaczy(3,trzy).
+znaczy(4,cztery).
+znaczy(5,piec).
+znaczy(6,szesc).
+znaczy(7,siedem).
+znaczy(8,osiem).
+znaczy(9,dziewiec).
+
+
 sklej([],X,X).
 sklej([X|L1],L2,[X|L3]) :-
     sklej(L1,L2,L3).
@@ -20,4 +32,44 @@ nieparzysta([_,_|R]):-
 parzysta([]).
 parzysta([_,_|R]):-
     parzysta(R).
+
+odwroc(L,R) :-
+     odwr(L,[],R).
+odwr([H|T],A,R) :-
+     odwr(T,[H|A],R).
+odwr([],A,A).
+
+palindrom(L):-
+    odwroc(L,X),
+    L = X.
+
+przesun([H|Tail],X):-
+    sklej(Tail,[H],X).
+
+przeloz([],[]).
+przeloz([H|Tail], [X|Rest]):-
+    znaczy(H,X),
+    przeloz(Tail,Rest).
+
+
+nalezy(X,[X|_]).
+nalezy(X,[_|Yogon]) :-
+	nalezy(X,Yogon).
+
+podzbior(L,[]).
+podzbior(L, [HL|TL]):-
+    nalezy(HL,L),
+    podzbior(L,TL).
+
+
+podziel([],[],[]).
+podziel([X],[X],[]).
+podziel([X],[],[X]).
+podziel([],X,Y).
+
+podziel([X],[X|_],Y).
+podziel([A,B|Tail],[A],[B]):-
+    podziel(Tail,[A|Rest],[B,Rest2]).
     
+    
+
